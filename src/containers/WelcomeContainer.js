9a753fa81e40ac572fe09c8659cae25dede27deb
hmalik88 <hbmalik88@gmail.com>
2019-04-10 06:06:21 -0400

    some more progress on the landing page

--- src/containers/WelcomeContainer.js
+++ src/containers/WelcomeContainer.js
@@ -5,17 +5,15 @@ import '../scss/Welcome.scss'
 const WelcomeContainer = () => {
   return(
     <>
-    <h1 className='welcome'>CollegeMatch</h1>
-      <p className='collegematch-intro'>
-        Analytics and tracking platform for<br/> making the match that <span className='maters'>maters</span>
-      </p>
-      <Link to="/login">
-        <div className='welcome-button'>
-        <span className='w-button'>
+      <h1 className='welcome'>CollegeMatch</h1>
+        <p className='collegematch-intro'>
+          Analytics and tracking platform for<br/> making the match that <span className='maters'>maters</span>
+        </p>
+      <div className='welcome-button'>
+        <Link to="/login">
         Please sign up or log in
-        </span>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </>
         )
 }
