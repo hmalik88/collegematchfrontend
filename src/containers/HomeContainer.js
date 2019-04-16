a50cc3244fafa0b7375eeef831ada1a364abe392
hmalik88 <hbmalik88@gmail.com>
2019-04-16 18:18:08 -0400

    finished with nav bar placement

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -41,9 +41,9 @@ export default class HomeContainer extends React.Component {
     return(
       <>
         <div id="home-nav">
-          <h2 id="home-option">Home</h2>
-          <h2 id="search-option">Search</h2>
           <h2 id="logout-option">Log Out</h2>
+          <h2 id="search-option">Search</h2>
+          <h2 id="home-option">Home</h2>
         </div>
         <h1 id="home-current-tracks">Current Tracks:</h1>
         <div id="carousel-container"></div>
