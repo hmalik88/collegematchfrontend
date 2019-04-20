354d087dd957a9ddf9213e78f603c89bf64343fa
hmalik88 <hbmalik88@gmail.com>
2019-04-20 19:53:51 -0400

    done styling/laying out left folder portion of tracks page

--- src/containers/CollegeTrackContainer.js
+++ src/containers/CollegeTrackContainer.js
@@ -54,9 +54,65 @@ export default class CollegeTrackContainer extends React.Component {
             <h1 id="tab-label2">Computer Science</h1>
           </div>
         </div>
-        <div id="track-folder-left">
-        </div>
-        <div id="track-folder-right">
+        <div id="top-folder">
+          <div id="track-folder-left">
+            <div id="left-folder-container">
+              <div id="track-pic-container"></div>
+              <div id="track-description-container"></div>
+              <div id="track-info-container">
+                <div>
+                  <label className="info-label">Application Fee:</label>
+                  <span className="college-info">$60</span>
+                </div>
+                <div>
+                  <label className="info-label">Application Deadline:</label>
+                  <span className="college-info">12/15/19</span>
+                </div>
+                <div>
+                  <label className="info-label">Early Action Deadline:</label>
+                  <span className="college-info">9/1/19</span>
+                </div>
+                <div>
+                  <label className="info-label">SAT Requirement:</label>
+                  <span className="college-info">1400</span>
+                </div>
+                <div>
+                  <label className="info-label">SAT Average:</label>
+                  <span className="college-info">1500</span>
+                </div>
+                <div>
+                  <label className="info-label">Undergraduate Size:</label>
+                  <span className="college-info">3,500</span>
+                </div>
+                <div>
+                  <label className="info-label">GPA Requirement:</label>
+                  <span className="college-info">4.0</span>
+                </div>
+                <div>
+                  <label className="info-label">Acceptance Rate:</label>
+                  <span className="college-info">5%</span>
+                </div>
+                <div>
+                  <label className="info-label">Avg. Federal Grant Aid:</label>
+                  <span className="college-info">$10,000</span>
+                </div>
+                <div>
+                  <label className="info-label">Avg. Financial Aid:</label>
+                  <span className="college-info">$3,000</span>
+                </div>
+                <div>
+                  <label className="info-label">Avg. Housing Cost:</label>
+                  <span className="college-info">$60</span>
+                </div>
+                <div>
+                  <label className="info-label">Avg. Cost of Attendance:</label>
+                  <span className="college-info">$60</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="track-folder-right">
+          </div>
         </div>
         <CreateModal />
       </>
